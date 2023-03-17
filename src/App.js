343dd9e71da31b91123f0748d075c7d755b2f617
Shanilka Liyanage <shanilka.liyanage@pronto.net>
2023-03-17 17:15:48 +0530

    load data from csv

--- src/App.js
+++ src/App.js
@@ -1,52 +1,102 @@
-import React from "react";
-// import fetch from "node-fetch"; // Import fetch from node-fetch module
+import React, { useState } from "react";
+import Papa from "papaparse";
 
-import "./App.css";
+// Allowed extensions for input file
+const allowedExtensions = ["csv"];
 
-function App() {
-  const callApi = () => {
-    const prompt =
-      'The followings are the location coordinates and priority (HIGH, LOW) of some service calls I have to engage. Can you give me the most efficient order I can visit them, always give the top priority to locations with "HIGH", my current location is (-38.103179, 145.225357). Always assume the newly visited location is my current location. Use google maps API to calculate the distance between locations. {LOC1, LOW,-38.08039743174168, 145.2792105223707381}, {LOC2, HIGH,-38.07508367670604, 145.249708711921282}, {LOC3, High,-38.08925283209689, 145.33321383641483}, {LOC4, LOW,-38.0648486902882, 145.3002118111653384}, {LOC5, LOW,-38.101648590578314, 145.32421328407344}. The given format is {Location name, Priority, latitude, longitude} Please create a location order list to this format {location name}. Only provide an array with locations. no extra strings.';
-    const temperature = 0.3;
-    const maxTokens = 256;
-    const topP = 1;
-    const frequencyPenalty = 0;
-    const presencePenalty = 0;
-    const model = "text-davinci-003";
-    const apiKey = "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";
+const App = () => {
+  const [data, setData] = useState([]);
 
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
+  const [error, setError] = useState("");
+  const [file, setFile] = useState("");
+
+  const handleFileChange = (e) => {
+    setError("");
+
+    // Check if user has entered the file
+    if (e.target.files.length) {
+      const inputFile = e.target.files[0];
+
+      const fileExtension = inputFile?.type.split("/")[1];
+      if (!allowedExtensions.includes(fileExtension)) {
+        setError("Please input a csv file");
+        return;
+      }
+
+      setFile(inputFile);
+    }
+  };
+  const handleParse = () => {
+    if (!file) return setError("Enter a valid file");
+
+    const reader = new FileReader();
+
+    reader.onload = async ({ target }) => {
+      const csv = Papa.parse(target.result, { header: true });
+      const parsedData = csv?.data;
+      console.log("PARSED DATA", parsedData);
+
+      setData(parsedData);
     };
+    reader.readAsText(file);
+  };
 
-    const data = {
-      model: model,
-      prompt: prompt,
-      temperature: temperature,
-      max_tokens: maxTokens,
-      top_p: topP,
-      frequency_penalty: frequencyPenalty,
-      presence_penalty: presencePenalty,
-    };
-    fetch("https://api.openai.com/v1/completions", {
-      method: "POST",
-      headers: headers,
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        const textData = json.choices[0].text;
-        console.log(textData);
-      })
-      .catch((err) => console.error(err));
+  const ServiceCallList = ({ serviceCalls }) => {
+    return (
+      <div>
+        {serviceCalls.map((serviceCall) => (
+          <div key={serviceCall["Service call no"]} className="card">
+            <div className="card-body">
+              <h5 className="card-title">
+                Service Call {serviceCall["Service call no"]}
+              </h5>
+              <p className="card-text">
+                <strong>Required Skill:</strong>{" "}
+                {serviceCall[" Service call required skill"]}
+              </p>
+              <p className="card-text">
+                <strong>Engineer:</strong> {serviceCall[" Enginner"]}
+              </p>
+              <p className="card-text">
+                <strong>Priority:</strong> {serviceCall[" Priority"]}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   };
 
   return (
     <div>
-      <button onClick={callApi}>Click Me</button>
+      <label htmlFor="csvInput" style={{ display: "block" }}>
+        Enter CSV File
+      </label>
+      <input
+        onChange={handleFileChange}
+        id="csvInput"
+        name="file"
+        type="File"
+      />
+      <div>
+        <button onClick={handleParse}>Parse</button>
+      </div>
+
+      <ServiceCallList serviceCalls={data} />
+
+      {/* <div className="container">
+        {data.map((value, index) => {
+          return (
+            <tr key={index}>
+              {value.map((val, i) => {
+                return <td key={i}>{val}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </div> */}
     </div>
   );
-}
+};
 
 export default App;
